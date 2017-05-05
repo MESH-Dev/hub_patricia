a21d3814bbd638d0135fc43adcf960923ceb1506
Shaun <shaun@meshfresh.com>
2017-05-05 18:43:21 -0400

    Resolve from last branch, add template for forms

--- template-form.php
+++ template-form.php
@@ -1,20 +1,20 @@
 <?php 
 
-/* Template Name: Default Template*/
+/* Template Name: Form Page*/
 
 get_header(); 
 
 ?>
 
 
-<main id="content" data-creator="shaun">
+<main id="content" data-creator="shaun" class="form-page">
 
 	<div class="container">
 		<div class="row">
 			<div class="columns-12">
 				<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-					<h1><?php //the_title(); ?></h1>
+					<h1><?php the_title(); ?></h1>
 
 					<?php the_content(); ?>
 
